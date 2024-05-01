667eff2d5db1202a0edc470cfb6cf2a7547e3576
Murenzi Paterne <murenzi419@gmail.com>
2024-05-01 15:01:36 +0200

    Chore (added colors, and typographic constants for the project)

--- constants/Typography.ts
+++ constants/Typography.ts
@@ -1,15 +1,123 @@
-import { StyleSheet } from "react-native";
+const Typography = {
+  heading: {
+    // headings
+    heading_1: {
+      fontFamily: "Bold",
+      fontSize: 48,
+    },
+    heading_2: {
+      fontFamily: "Bold",
+      fontSize: 40,
+    },
+    heading_3: {
+      fontFamily: "Bold",
+      fontSize: 32,
+    },
+    heading_4: {
+      fontFamily: "Bold",
+      fontSize: 24,
+    },
+    heading_5: {
+      fontFamily: "Bold",
+      fontSize: 20,
+    },
+  },
 
-const Typography = StyleSheet.create({
-  // headings
-  heading_1: {
-    fontFamily: "Bold",
-    fontSize: 48,
+  bold: {
+    // body
+    bold_xLarge: {
+      fontFamily: "Bold",
+      fontSize: 18,
+    },
+    bold_large: {
+      fontFamily: "Bold",
+      fontSize: 16,
+    },
+    bold_medium: {
+      fontFamily: "Bold",
+      fontSize: 14,
+    },
+    bold_small: {
+      fontFamily: "Bold",
+      fontSize: 12,
+    },
+    bold_xSmall: {
+      fontFamily: "Bold",
+      fontSize: 10,
+    },
   },
-  heading_2: {
-    fontFamily: "Bold",
-    fontSize: 40,
+
+  semiBold: {
+    // semibold
+    semiBold_xLarge: {
+      fontFamily: "SemiBold",
+      fontSize: 18,
+    },
+    semiBold_large: {
+      fontFamily: "SemiBold",
+      fontSize: 16,
+    },
+    semiBold_medium: {
+      fontFamily: "SemiBold",
+      fontSize: 14,
+    },
+    semiBold_small: {
+      fontFamily: "SemiBold",
+      fontSize: 12,
+    },
+    semiBold_xSmall: {
+      fontFamily: "SemiBold",
+      fontSize: 10,
+    },
   },
-});
+
+  medium: {
+    // medium
+    medium_xLarge: {
+      fontFamily: "SemiBold",
+      fontSize: 18,
+    },
+    medium_large: {
+      fontFamily: "SemiBold",
+      fontSize: 16,
+    },
+    medium_medium: {
+      fontFamily: "SemiBold",
+      fontSize: 14,
+    },
+    medium_small: {
+      fontFamily: "SemiBold",
+      fontSize: 12,
+    },
+    medium_xSmall: {
+      fontFamily: "SemiBold",
+      fontSize: 10,
+    },
+  },
+
+  regular: {
+    // Regular
+    regular_xLarge: {
+      fontFamily: "Regular",
+      fontSize: 18,
+    },
+    regular_large: {
+      fontFamily: "Regular",
+      fontSize: 16,
+    },
+    regular_medium: {
+      fontFamily: "Regular",
+      fontSize: 14,
+    },
+    regular_small: {
+      fontFamily: "Regular",
+      fontSize: 12,
+    },
+    regular_xSmall: {
+      fontFamily: "Regular",
+      fontSize: 10,
+    },
+  },
+};
 
 export default Typography;
