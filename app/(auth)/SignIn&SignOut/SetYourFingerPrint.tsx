d872d6e499311495800a821ff971ba4ad2587eb1
Murenzi Paterne <murenzi419@gmail.com>
2024-05-01 13:04:28 +0200

    Chore (setup project structure. Worked on colors, constants, and icons)

---
+++ app/(auth)/SignIn&SignOut/SetYourFingerPrint.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "@/components/Themed";
+import { LeftArrow } from "@/components/UI/icons";
+import { SvgUri, SvgXml } from "react-native-svg";
+
+export default function SetYourFingerPrint() {
+  return (
+    <>
+      <Text>Set Your Finger Print</Text>
+      <LeftArrow fillColor="#212121" />
+    </>
+  );
+}
