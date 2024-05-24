df50419387b1e6563a0eb8bc4f5d3ebdf44e9849
mbabazi12 <mbabazi069@gmail.com>
2024-05-24 13:16:12 +0200

    feat: all articles screens added

--- app/(app)/Articles/_layout.tsx
+++ app/(app)/Articles/_layout.tsx
@@ -1,9 +1,12 @@
+import React from "react";
 import { Stack } from "expo-router";
 
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{headerShown:false}} />
+      <Stack.Screen name="ArticleSeeAll" options={{headerShown:false}} />
+      <Stack.Screen name="ArticleDetails" options={{headerShown:false}} />
     </Stack>
   );
 }
