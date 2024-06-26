6a835e770ef512a125023c3ca525daad47bda849
23nosurrend <keynesbizimana@gmail.com>
2024-06-26 15:00:31 +0200

    bg:add sentry setup to check apk logs

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Loading from "@/app/spinner/Loading";
-import { router } from "expo-router";
-import { useEffect } from "react";
+import { router, Slot, useNavigationContainerRef } from "expo-router";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { ThemeContext } from "@/ctx/ThemeContext";
 import { Colors } from "@/constants/Colors";
 import Typography from "@/constants/Typography";
 import { StatusBar } from "expo-status-bar";
-import BookingAppointment from "./(app)/ActionMenu/Booking/BookingAppointment";
-import Chips from "@/components/UI/ChipsComponent";
+import * as Sentry from '@sentry/react-native';
+import { isRunningInExpoGo } from 'expo';
 
-export default function SplashScreen() {
-  const { theme, changeTheme } = useContext(ThemeContext);
-  useEffect(() => {
-    setTimeout(() => {
-      // router.push("(auth)/ForgotPassword&Reset");
-      router.push("onBoarding");
-    },3000);
-  }, []);
-  
+const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+
+Sentry.init({
+  dsn: 'https://ed50d6a5ea43c4e35a082579c093f726@o4507497432154112.ingest.us.sentry.io/4507497451945984',
+  debug: true, 
+  integrations: [
+    new Sentry.ReactNativeTracing({
+    
+      routingInstrumentation,
+      enableNativeFramesTracking: !isRunningInExpoGo(),
+     
+    }),
+  ],
+});
+
+function SplashScreen() {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <>
       <StatusBar style={theme === "light" ? "dark" : "light"} />
@@ -28,42 +36,60 @@ export default function SplashScreen() {
           width: "100%",
           justifyContent: "center",
           alignItems: "center",
-          backgroundColor:
-            theme === "light" ? Colors.others.white : Colors.dark._1,
+          backgroundColor: theme === "light" ? Colors.others.white : Colors.dark._1,
         }}
       >
         <View style={{ flexDirection: "row", gap: 10, alignItems: "center" }}>
           <View>
-            <View>
-              <Image source={require("../assets/images/icon.png")} />
-            </View>
+            <Image source={require("../assets/images/icon.png")} />
           </View>
-
-          <View>
-            <Text
-              style={[
-                Typography.heading._1,
-                {
-                  color:
-                    theme === "light"
-                      ? Colors.grayScale._900
-                      : Colors.others.white,
-                },
-              ]}
-            >
-              Medica
-            </Text>
-          </View>
+          <Text
+            style={[
+              Typography.heading._1,
+              {
+                color: theme === "light" ? Colors.grayScale._900 : Colors.others.white,
+              },
+            ]}
+          >
+            Medica
+          </Text>
         </View>
-
         <View style={styles.spin}>
           <Loading />
         </View>
       </View>
-      {/* <BookingAppointment /> */}
     </>
   );
 }
+
+function RootLayout() {
+  const [isSplashVisible, setSplashVisible] = useState(true);
+  const ref = useNavigationContainerRef();
+
+  useEffect(() => {
+    if (ref) {
+      routingInstrumentation.registerNavigationContainer(ref);
+    }
+  }, [ref]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSplashVisible(false);
+      router.push("onBoarding");
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isSplashVisible) {
+    return <SplashScreen />;
+  }
+
+  return <Slot />;
+}
+
+
+export default Sentry.wrap(RootLayout);
 
 const styles = StyleSheet.create({
   text: {
