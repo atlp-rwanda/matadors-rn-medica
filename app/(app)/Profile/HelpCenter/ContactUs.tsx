020ed99503afddef60c72f9ee041fabcb2b4e42c
Murenzi Paterne <murenzi419@gmail.com>
2024-07-05 14:45:11 +0200

    feat: added linking to the links in the contact us section

--- app/(app)/Profile/HelpCenter/ContactUs.tsx
+++ app/(app)/Profile/HelpCenter/ContactUs.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   FacebookBlueIcon,
   HeadSetBlueIcon,
@@ -13,6 +14,8 @@ import { useContext } from "react";
 import { ScrollView } from "react-native";
 import { View } from "react-native";
 import { SvgXml } from "react-native-svg";
+import { openBrowserAsync } from "expo-web-browser";
+import * as Linking from "expo-linking";
 
 export default function ContactUs() {
   const { theme } = useContext(ThemeContext);
@@ -33,32 +36,42 @@ export default function ContactUs() {
     >
       <View style={{ paddingHorizontal: 20, gap: 20 }}>
         <ContactsListing
-          onPress={() => {}}
+          onPress={() => Linking.openURL("tel:+250723908534 ")}
           icon={() => <SvgXml xml={HeadSetBlueIcon} />}
           title="Customer Service"
         />
         <ContactsListing
-          onPress={() => {}}
+          onPress={() => {
+            Linking.openURL("whatsapp://send?text=hello&phone=+250782750607");
+          }}
           icon={() => <SvgXml xml={WhatsappBlueIcon} />}
           title="Whatsapp"
         />
         <ContactsListing
-          onPress={() => {}}
+          onPress={() => {
+            openBrowserAsync("https://google.com");
+          }}
           icon={() => <SvgXml xml={WebsiteBlueIcon} />}
           title="Website"
         />
         <ContactsListing
-          onPress={() => {}}
+          onPress={() => {
+            openBrowserAsync("https://facebook.com");
+          }}
           icon={() => <SvgXml xml={FacebookBlueIcon} />}
           title="Facebook"
         />
         <ContactsListing
-          onPress={() => {}}
+          onPress={() => {
+            openBrowserAsync("https://twitter.com");
+          }}
           icon={() => <SvgXml xml={TwitterBlueIcon} />}
           title="Twitter"
         />
         <ContactsListing
-          onPress={() => {}}
+          onPress={() => {
+            openBrowserAsync("https://instagram.com");
+          }}
           icon={() => <SvgXml xml={InstagramBlueIcon} />}
           title="Instagram"
         />
